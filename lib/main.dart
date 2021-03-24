ae5cd6ec2ab60bc27fb5e641c23b5553c32b2b74
ToBeNew <crebiz76@gmail.com>
2021-03-25 01:52:49 +0900

    Center & Column - mainAxisAlignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 // 1. Flutter material library import
-import 'package:flutter/material.dart';
-
 // 2. App start point: main()
 // 3. Calling function: runApp()
 // 4. Argument: MyApp()
-void main() => runApp(MyApp());
-
 // 5. Create custome widget(stl)
 // 6. Substitution return widget(Container -> MaterialApp)
 // 7. title description
@@ -13,21 +9,41 @@ void main() => runApp(MyApp());
 // 9. primarySwatch argument in ThemeData
 // 10. Color selection
 // 11. home description
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'First app',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('First app in Text'),
-          ),
-          body: Center(
-            child: Column(
-              children: <Widget>[Text('Hello'), Text('World'), Text('Hello')],
-            ),
-          ),
-        ));
+      title: 'Character card',
+      home: MyCard(),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DOHYUN'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        // elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+          ],
+        ),
+      ),
+    );
   }
 }
