effe68850afc5fa55aa1d908be931e4b4a62d6aa
ToBeNew <crebiz76@gmail.com>
2021-03-29 23:06:45 +0900

    Icon menu complete

---
+++ lib/icon_menu.dart
@@ -0,0 +1,50 @@
+﻿import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AppBar',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: MyPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar Icon Menu'),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('Menu button is clicked');
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search button is clicked');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
