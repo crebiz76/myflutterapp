fe6df79d0152fcdceeb83e80c3691650dc67c9ed
ToBeNew <crebiz76@gmail.com>
2021-04-05 23:41:07 +0900

    myflutterapp release

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,116 @@
-﻿// class Circle(){}
+﻿import 'package:flutter/material.dart';
 
-// class Square(){}
+void main() => runApp(MyApp());
 
-// class SquareSlot(){
-//   insert (Square squareSlot){
-
-//   }
-// }
-
-// class CircleSlot(){
-//   insert (Circle circleSlot){
-
-//   }
-// }
-
-class Circle(){}
-
-class Square(){}
-
-class Slot<T>{
-  insert(T shape){
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AppBar',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: MyPage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
-void main() {
-  var circleSlot = new Slot<Circle>();
-  circleSlot.insert(new Circle());
-  
-  var squareSlot = new Slot<Square>();
-  circleSlot.insert(new Square());
 
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar Icon Menu'),
+        centerTitle: true,
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search button is clicked');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/baby1.png'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/baby2.png'),
+                  backgroundColor: Colors.white,
+                )
+              ],
+              accountName: Text('Do-hyun'),
+              accountEmail: Text('dohyun@gong.com'),
+              onDetailsPressed: () {
+                print('Arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  )),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: () {
+                print('Home is clicked');
+              },
+              trailing: Icon(
+                Icons.add,
+                color: Colors.grey[850],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('Settings'),
+              onTap: () {
+                print('Settings is clicked');
+              },
+              trailing: Icon(
+                Icons.add,
+                color: Colors.grey[850],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: Icon(
+                Icons.add,
+                color: Colors.grey[850],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
